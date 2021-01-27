b05198e507265df3cc18ef36ea7ee2211b212d42
z0mbi19 <32523746+z0mbi19@users.noreply.github.com>
2021-01-27 16:23:01 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-white text-blue container">
+        
+        <q-toolbar-title>
+          <q-btn @click="$router.push('/') " >
+            <img src="../assets/logo.png" class="logo" />
+          </q-btn>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-footer elevated>
+        <q-toolbar class="bg-white text-blue container">
+          <q-toolbar-title class="Endereço">Endereço: Q SRTV/SUL QUADRA 701 CONJUNTO L LOTE 38 <br/> CNPJ:37.311.173/0001-30 </q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+</script>
+
+<style>
+a{
+  text-decoration: none;
+}
+.logo{
+  height: 40px;
+}
+.Endereço{
+  text-align: center;
+  font-size: 15px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page-container class="flex flex-center">
+    
+    <div class="row" id="home">
+      <div class="col-6 ">
+        <h3 class="texto-principla" >Consultancy and software factory</h3>
+        <div class="row " >
+          <q-btn class="btn-talk">
+            <a href="mailto:victor@qcedtech.com" >Talk to us</a>
+          </q-btn>
+
+        </div>
+      </div>
+      <div class="col-5"  >
+        <img src="../assets/international.png" width="90%"/>
+      </div>
+        <div class="">
+          
+        </div>
+    </div>
+  </q-page-container>
+  
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style>
+div {
+  padding: 10px;
+}
+.texto-principla{
+  text-align: center;
+}
+#home{
+  display: flex;
+  align-items: center;
+}
+.btn-talk{
+  display: flex;
+  align-content: center;
+  width: 100%;
+}
+</style>
